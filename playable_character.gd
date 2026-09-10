77b8b0f859a511a63f667834156d2950ca8dc0a8
clair-green2048 <clair.green2048@gmail.com>
2026-09-09 20:37:10 -0400

    Adding brake lights and better steering

--- playable_character.gd
+++ playable_character.gd
@@ -4,6 +4,10 @@ extends CharacterBody3D
 # use to turn car tires
 @export var frontLeftWheel : CSGBox3D
 @export var frontRightWheel: CSGBox3D
+@export var leftBrakeLight: SpotLight3D
+@export var rightBrakeLight: SpotLight3D
+@export var leftBrakeLightNode: CSGCylinder3D
+@export var rightBrakeLightNode: CSGCylinder3D
 
 const MAX_SPEED = 25.0
 const MAX_WHEEL_ANGLE = 3.14159 / 6
@@ -19,7 +23,12 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	leftBrakeLight.visible = Input.is_action_pressed("Move Backward") and speed > 0.0
+	rightBrakeLight.visible = Input.is_action_pressed("Move Backward") and speed > 0.0
+	(leftBrakeLightNode.material as StandardMaterial3D).emission_enabled = Input.is_action_pressed("Move Backward") and speed > 0.0
+	(rightBrakeLightNode.material as StandardMaterial3D).emission_enabled = Input.is_action_pressed("Move Backward") and speed > 0.0
+	
 	# Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		#velocity.y = JUMP_VELOCITY
@@ -30,16 +39,28 @@ func _physics_process(delta: float) -> void:
 	
 	var angle = 0.0
 	var deltaAngle = 0.0
+	var requestedDeltaAngle = 0.0
+	
 	if Input.is_action_pressed("Turn Left"):
 		angle += delta * ANGLE_ACCEL
-		deltaAngle += delta * WHEEL_ACCEL
+		requestedDeltaAngle += delta * WHEEL_ACCEL
 		
 	if Input.is_action_pressed("Turn Right"):
 		angle -= delta * ANGLE_ACCEL
-		deltaAngle -= delta * WHEEL_ACCEL
+		requestedDeltaAngle -= delta * WHEEL_ACCEL
 		
-	# function rotations around first arg axis by second arg angle
-	#rotate(basis.y, angle)
+	var newWheelAngle = clamp(
+		wheelAngle + requestedDeltaAngle,
+		-MAX_WHEEL_ANGLE,
+		MAX_WHEEL_ANGLE
+	)
+
+	var actualDeltaAngle = newWheelAngle - wheelAngle
+	wheelAngle = newWheelAngle
+		
+	frontLeftWheel.rotate(frontLeftWheel.basis.y.normalized(), actualDeltaAngle)
+	frontRightWheel.rotate(frontLeftWheel.basis.y.normalized(), actualDeltaAngle)
+
 	
 	# negative z is the default forward vector
 	var direction := -basis.z
@@ -52,6 +73,7 @@ func _physics_process(delta: float) -> void:
 		
 	if Input.is_action_pressed("Move Backward"):
 		speed -= delta * ACCEL
+
 		
 	
 	if speed > 0.0:
@@ -67,17 +89,6 @@ func _physics_process(delta: float) -> void:
 		speed = MAX_SPEED
 	if speed < -MAX_SPEED:
 		speed = -MAX_SPEED
-		
-	if abs(wheelAngle + deltaAngle) > MAX_WHEEL_ANGLE:
-		if deltaAngle < 0.0:
-			deltaAngle = -(wheelAngle + MAX_WHEEL_ANGLE)
-			wheelAngle = -MAX_WHEEL_ANGLE
-		else:
-			deltaAngle = MAX_WHEEL_ANGLE - deltaAngle
-			wheelAngle = MAX_WHEEL_ANGLE
-	
-	frontLeftWheel.rotate(frontLeftWheel.basis.y.normalized(), deltaAngle)
-	frontRightWheel.rotate(frontLeftWheel.basis.y.normalized(), deltaAngle)
 	
 	direction *= speed
 	direction.y = velocity.y
